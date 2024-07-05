543134b88b1e3c8c16fa14126affe323a0214206
Logi4Owl <147890114+Logi4Owl@users.noreply.github.com>
2024-07-05 04:34:46 +0000

    add locale language zh_tw on i18n

--- client/src/utils/timeago.ts
+++ client/src/utils/timeago.ts
@@ -1,7 +1,7 @@
 import { format } from "@astroimg/timeago";
-import i18n from "i18next"
+import i18n from "i18next";
 
 export function timeago(time: string | number | Date) {
-    const locale = i18n.language == 'zh' ? 'zh-CN' : 'en'
-    return format(time, "DEFAULT", locale)
+    const locale = i18n.language === 'zh_tw' ? 'zh-TW' : (i18n.language === 'zh_cn' ? 'zh-CN' : 'en');
+    return format(time, "DEFAULT", locale);
 }
